7a7288d6f2cc4d82b370a3256f470f61cb1319bf
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2026-01-05 21:32:04 +0100

    fix: comment out empty barrel exports to fix production build

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,6 @@
-export * from "./ui";
-export * from "./calendar";
-export * from "./layout";
+// Barrel exports - uncomment as components are created
+// export * from "./ui";
+// export * from "./calendar";
+// export * from "./layout";
+
+export {};
