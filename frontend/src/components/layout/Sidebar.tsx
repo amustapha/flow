50810fb23d4b5dcf0eb27ddf0087f3837b4e762b
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2026-01-06 01:24:12 +0100

    feat: add bell icon to create reminder button

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { BellIcon } from '@heroicons/react/24/outline';
 import { Calendar, Button } from '@/components/ui';
 
 export interface SidebarProps {
@@ -28,7 +29,8 @@ export function Sidebar({ onCreateReminder }: SidebarProps) {
           onClick={onCreateReminder}
           className="w-full"
         >
-          + Create Reminder
+          <BellIcon className="h-5 w-5 mr-2" />
+          Create Reminder
         </Button>
       </div>
     </div>
